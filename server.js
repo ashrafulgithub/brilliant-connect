c88f79dc1e5f84f9631c84c2b1524780d37fc174
Ashraful Islam <61355941+ashrafulgithub@users.noreply.github.com>
2025-08-12 02:18:40 +0600

    Backend initial setup

--- server.js
+++ server.js
@@ -1,1 +1,1 @@
-﻿const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('Brilliant Connect Backend Running')); app.listen(3000, () => console.log('Server started on port 3000'));
+﻿const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('Brilliant Connect Backend Running')); app.listen(3000, () => console.log('Server started on port 3000'))
